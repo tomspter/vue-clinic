82dd8eaba5ab0ee2fb0f1866d07b6ce50d54c51f
tomspter <2501760110@qq.com>
2019-06-29 11:14:25 +0800

    Initial commit

---
+++ src/router/modules/components.js
@@ -0,0 +1,24 @@
+/** When your routing table is too long, you can split it into small modules **/
+
+import Layout from '@/layout'
+
+const componentsRouter = {
+  path: '/components',
+  component: Layout,
+  redirect: 'noRedirect',
+  name: 'ComponentDemo',
+  meta: {
+    title: 'Components',
+    icon: 'component'
+  },
+  children: [
+    {
+      path: 'mixin',
+      component: () => import('@/views/components-demo/mixin'),
+      name: 'ComponentMixinDemo',
+      meta: { title: 'Component Mixin' }
+    }
+  ]
+}
+
+export default componentsRouter
